1900cc24a6193fde77143f10b91112cbf82bd8a7
liuwen <liuwen@fangdd.com>
2017-02-26 22:53:36 +0800

    conversation agent

--- mouth/audio/audio_input_test.py
+++ mouth/audio/audio_input_test.py
@@ -6,6 +6,15 @@ import wave
 import os
 from asr import asrer
 
+
+
+NUM_SAMPLES = 24000      # pyAudio内部缓存的块的大小
+SAMPLING_RATE = 48000    # 取样频率
+LEVEL = 32780         # 声音保存的阈值
+COUNT_NUM = 18          # NUM_SAMPLES个取样之内出现COUNT_NUM个大于LEVEL的取样则记录声音
+SAVE_LENGTH = 8         # 声音记录的最小长度：SAVE_LENGTH * NUM_SAMPLES 个取样
+
+
 # 开启声音输入
 pa = PyAudio()
 stream = pa.open(format=paInt16, channels=1, rate=SAMPLING_RATE, input=True,
